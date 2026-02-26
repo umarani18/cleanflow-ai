e4fa0745efd327d45db2a2d0bbb89cd57314c1a1
Noel-Teens <teeniejoshua@gmail.com>
2026-02-26 18:46:41 +0530

    feat(quarantine): export quarantine editor from files module

--- modules/files/index.ts
+++ modules/files/index.ts
@@ -19,6 +19,7 @@ export { PushToERPModal } from "./components/push-to-erp-modal"
 export { ColumnProfilingPanel } from "./components/column-profiling-panel"
 export { ColumnExportDialog, ColumnExportContent } from "./components/column-export-dialog"
 export { default as CustomDestinationExport } from "./components/custom-destination-export"
+export { QuarantineEditorDialog } from "./components/quarantine-editor"
 
 export type * from "./types"
 export { useFileManager } from "./hooks/use-file-manager"
